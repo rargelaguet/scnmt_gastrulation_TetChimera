7ae8506b3ea1ec82d2d5517f5cd6131031b437c0
rargelaguet <ricard.argelaguet@gmail.com>
2021-12-12 19:05:23 +0100

    foo

--- metacc/differential/feature_level/run.R
+++ metacc/differential/feature_level/run.R
@@ -12,9 +12,8 @@ opts$group_label <- "celltype_class"
 
 opts$min_cells <- 10
 
-
 # Genomic contexts
-opts$annos <- c("prom_2000_2000")
+opts$annos <- c("multiome_peaks")
 # opts$annos <- NULL
 # if (is.null(opts$annos)) {
 #   opts$annos <- list.files(io$met_data_parsed, pattern=".tsv.gz") %>% gsub(".tsv.gz","",.)# %>% head(n=3)
@@ -34,47 +33,51 @@ sample_metadata <- sample_metadata %>%
   setnames(opts$group_label,"group")
 
 # subset groups with sufficient number of cells
-sample_metadata <- sample_metadata %>%
-  .[,N:=.N,by="group"] %>% .[N>=opts$min_cells] %>% .[,N:=NULL]
+# sample_metadata <- sample_metadata %>%
+#   .[,N:=.N,by="group"] %>% .[N>=opts$min_cells] %>% .[,N:=NULL]
 
-table(sample_metadata$group)
 
 #########
 ## Run ##
 #########
 
-opts$celltypes <- unique(sample_metadata$celltype.mapped_mnn) %>% head(n=2)
+opts$celltypes <- unique(sample_metadata$celltype.mapped_mnn)
 
-io$outdir <- file.path(io$basedir,sprintf("results/met/differential/%s",opts$group_label))
+io$outdir <- c(
+  "CG" = file.path(io$basedir,sprintf("results/met/differential/%s",opts$group_label)),
+  "GC" = file.path(io$basedir,sprintf("results/acc/differential/%s",opts$group_label))
+)
 
-# i <- "Blood_progenitors_2"; anno <- "prom_2000_2000"
+# i <- "Blood_progenitors_2"; anno <- "prom_2000_2000"; context <- "CG"
 for (i in opts$celltypes) {
   tmp <- sample_metadata[celltype.mapped_mnn==i,.N,by="group"]
-  if (all(tmp$N>=opts$min_cells)) {
+  print(i); print(tmp)
+  if (all(tmp$N>=opts$min_cells) & nrow(tmp)>1) {
     groupA <- tmp$group[[1]]; groupB <- tmp$group[[2]]
+    for (context in c("CG","GC")) {
+      for (anno in opts$annos) {
+        outfile <- file.path(io$outdir[[context]],sprintf("%s_%s_vs_%s.txt.gz", anno, groupA, groupB))
+        if (file.exists(outfile)) {
+          print(sprintf("%s already exists, skipping...",outfile))
+        } else {
     
-    for (anno in opts$annos) {
-      
-      outfile <- file.path(io$outdir,sprintf("%s_%s_vs_%s.txt.gz", anno, groupA, groupB))
-      if (file.exists(outfile)) {
-        print(sprintf("%s already exists, skipping...",outfile))
-      } else {
-  
-        # Define LSF command
-        if (grepl("BI",Sys.info()['nodename'])) {
-          lsf <- ""
-        } else if (grepl("pebble|headstone", Sys.info()['nodename'])) {
-          lsf <- sprintf("sbatch -n 1 --mem 7G --wrap")
+          # Define LSF command
+          if (grepl("BI",Sys.info()['nodename'])) {
+            lsf <- ""
+          } else if (grepl("pebble|headstone", Sys.info()['nodename'])) {
+            lsf <- sprintf("sbatch -n 1 --mem 7G --wrap")
+          }
+          cmd <- sprintf("%s 'Rscript %s --groupA %s --groupB %s --anno %s --group_label %s --context %s --min_cells %d --outfile %s'", 
+            lsf, io$script, groupA, groupB, anno, opts$group_label, context, opts$min_cells, outfile)
+    
+          # Run
+          print(cmd)
+          system(cmd)
+          
         }
-        cmd <- sprintf("%s 'Rscript %s --groupA %s --groupB %s --anno %s --group_label %s --context CG --min_cells %d --outfile %s'", 
-          lsf, io$script, groupA, groupB, anno, opts$group_label, opts$min_cells, outfile)
-  
-        # Run
-        print(cmd)
-        system(cmd)
-        
       }
     }
+    cat("\n\n")
   }
   
 }
